b4faf5a8dfa16795de6cd09b9ae3c1461f4d38aa
EugeneDashko <eugenedashko@yahoo.com>
2022-10-26 22:41:55 +0300

    finish section video

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,15 @@ flsFunction.isWepb();
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 const swiper = new Swiper()
+
+
+(function() {
+    const header = document.querySelector('header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header_active');
+        } else {
+            header.classList.remove('header_active')
+        }
+    };
+}());
